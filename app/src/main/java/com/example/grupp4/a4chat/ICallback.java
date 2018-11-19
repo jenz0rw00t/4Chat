82c8a1e15a1669a1cc17b9c6c0c28cca820d7977
willbback <william.alistairbrown@yahoo.com>
2018-11-19 14:04:14 +0100

    a

---
+++ app/src/main/java/com/example/grupp4/a4chat/ICallback.java
@@ -0,0 +1,10 @@
+package com.example.grupp4.a4chat;
+
+/**
+ * Created by User on 6/4/2018.
+ */
+
+public interface ICallback {
+
+    void done(Exception e);
+}
---
+++ app/src/main/java/com/example/grupp4/a4chat/IMainActivity.java
@@ -0,0 +1,36 @@
+package com.example.grupp4.a4chat;
+
+import android.graphics.drawable.Drawable;
+
+public interface IMainActivity {
+
+    void setCameraFrontFacing();
+
+    void setCameraBackFacing();
+
+    boolean isCameraFrontFacing();
+
+    boolean isCameraBackFacing();
+
+    void setFrontCameraId(String cameraId);
+
+    void setBackCameraId(String cameraId);
+
+    String getFrontCameraId();
+
+    String getBackCameraId();
+
+    void hideStatusBar();
+
+    void showStatusBar();
+
+    void hideStillshotWidgets();
+
+    void showStillshotWidgets();
+
+    void toggleViewStickersFragment();
+
+    void addSticker(Drawable sticker);
+
+    void setTrashIconSize(int width, int height);
+}

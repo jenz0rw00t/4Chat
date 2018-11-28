599efa4568bded85e8b57050744b8499efa3a578
jenz0rw00t <l0senordGHw00t>
2018-11-28 15:01:47 +0100

    CropCircle bugfix

--- app/src/main/java/com/iths/grupp4/a4chat/allusers/AllUserProfileFragment.java
+++ app/src/main/java/com/iths/grupp4/a4chat/allusers/AllUserProfileFragment.java
@@ -26,6 +26,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.squareup.picasso.Picasso;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 import java.util.HashMap;
 import java.util.Map;
